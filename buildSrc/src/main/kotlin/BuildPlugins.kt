2ff1ffa73c72f6a7462d61f5c9cdded437d4b205
david <hirakoshinji722@gmail.com>
2020-11-22 16:29:13 +0100

    add core module

--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -3,6 +3,7 @@
  */
 object BuildPlugins {
     const val android_application = "com.android.application"
+    const val android_library = "com.android.library"
     const val spotless = "plugins.spotless"
     const val ktlint = "plugins.ktlint"
     const val git_hooks = "plugins.git-hooks"
---
+++ core/build.gradle.kts
@@ -0,0 +1,46 @@
+
+import BuildAndroidConfig.build_tools_version
+import BuildAndroidConfig.compile_sdk_version
+import BuildAndroidConfig.minimum_sdk_version
+import BuildAndroidConfig.target_sdk_version
+import BuildAndroidConfig.test_instrumentation_runner
+import BuildAndroidConfig.version_code
+import BuildAndroidConfig.version_name
+import dependencies.BuildDependencies
+
+plugins{
+    id(BuildPlugins.android_library)
+    kotlin(BuildPlugins.kotlin_android)
+    kotlin(BuildPlugins.kotlin_kapt)
+    kotlin(BuildPlugins.kotlin_android_extention)
+}
+
+android {
+    compileSdkVersion(compile_sdk_version)
+    buildToolsVersion(build_tools_version)
+
+    defaultConfig {
+        minSdkVersion(minimum_sdk_version)
+        targetSdkVersion(target_sdk_version)
+        versionCode = version_code
+        versionName = version_name
+
+        testInstrumentationRunner = test_instrumentation_runner
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+}
+
+dependencies {
+    implementation( fileTree( mapOf( "dir" to "libs", "include" to  listOf("*.jar")  )))
+
+    implementation(BuildDependencies.kotlin)
+}
